2eb72f125ae68315909f99cd61cb44f7b8724070
LovelyToaster <92214385+LovelyToaster@users.noreply.github.com>
2021-10-10 08:46:42 +0800

    Update C-Study-20211009.c

--- C-Study-20211009.c
+++ C-Study-20211009.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #include<math.h>
+#include<stdlib.h>
 int main()
 {
     int a,b,c,d;
@@ -9,34 +10,37 @@ int main()
     printf("input 4./ \n");
     printf("input 5.END \n");
     scanf("%d" ,&d);
-    switch (d)
+    if (d>0&&d<=5)
     {
-    case 1:printf("input a b:\n");
+        switch (d)
+        {
+           case 1:printf("input a b:\n");
            scanf("%d%d" ,&a,&b);
            c=a+b;
            printf("output: %d\n" ,c);
            break;
-    case 2:printf("input a b:\n");
+           case 2:printf("input a b:\n");
            scanf("%d%d" ,&a,&b);
            c=a-b;
            printf("output: %d\n" ,c);
            break;
-    case 3:printf("input a b:\n");
+           case 3:printf("input a b:\n");
            scanf("%d%d" ,&a,&b);
            c=a*b;
            printf("output: %d\n" ,c);
            break;
-    case 4:printf("input a b:\n");
+           case 4:printf("input a b:\n");
            scanf("%d%d" ,&a,&b);
            c=a/b;
            printf("output: %d\n" ,c);
            break; 
-    case 5:return 0;     
+           case 5:return 0;
+        }
     }
-    if (d>5)
+    else
     {
-           printf("NO THIS CHOOSE\n");
+        printf("NO THIS CHOOSE\n");
     }
+    system("pause");
     return 0;
-    
 }
