381790d3beed336d2adf45150e9927435c53d717
LovelyToaster <92214385+LovelyToaster@users.noreply.github.com>
2021-10-19 08:08:51 -0500

    判断圆在不在矩形里面

---
+++ C++-Homework-6-20211019.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int W,H,x,y,r;
+    scanf("%d%d%d%d%d",&W,&H,&x,&y,&r);
+    if(x+r>W||x-r<0||y+r>H||y-r<0)
+    printf("No\n");
+    else
+    printf("Yes\n");
+    return 0;
+}
