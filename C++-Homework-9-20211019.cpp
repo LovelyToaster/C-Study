c42feadf18b1ad4693c1c1f188405581e1d8e7f9
LovelyToaster <92214385+LovelyToaster@users.noreply.github.com>
2021-10-19 08:09:51 -0500

    判断直角三角形

---
+++ C++-Homework-9-20211019.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a+b>c&&a+c>b&&b+c>a)
+    {
+        if(a*a+b*b==c*c||a*a+c*c==b*b||b*b+c*c==a*a)
+        printf("yes\n");
+        else
+        printf("no\n");
+    }
+    else
+    printf("not a triangle\n");
+    return 0;     
+}
