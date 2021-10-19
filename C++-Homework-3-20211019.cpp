1bfc64a346d7840436c8a915797d202d8ec80671
LovelyToaster <92214385+LovelyToaster@users.noreply.github.com>
2021-10-18 22:33:56 -0500

    输入三个数，输出最大的

---
+++ C++-Homework-3-20211019.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b&&a>c)
+    printf("%d",a);
+    if(b>a&&b>c)
+    printf("%d",b);
+    if(c>a&&c>b)
+    printf("%d",c);
+    return 0;
+}
