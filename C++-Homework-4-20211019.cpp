f36e4fe6ad49459bb5bd3bb55a156b972ac2a87d
LovelyToaster <92214385+LovelyToaster@users.noreply.github.com>
2021-10-18 22:34:29 -0500

    符号函数

---
+++ C++-Homework-4-20211019.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x;
+    scanf("%d",&x);
+    if(x<0)
+    printf("-1\n");
+    if(x==0)
+    printf("0\n");
+    if(x>0)
+    printf("1\n");
+    return 0;
+}
