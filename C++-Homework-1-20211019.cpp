e74dc96fccd857d9f5358e5e8e998c33486627d8
LovelyToaster <92214385+LovelyToaster@users.noreply.github.com>
2021-10-18 22:32:59 -0500

    水仙花数

---
+++ C++-Homework-1-20211019.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,a,b,c,x;
+    while(scanf("%d",&n)!=EOF)
+    {
+     if(n==0) 
+     return 0;
+     else
+     a=n/100;
+     b=n/10%10;
+     c=n%10;
+     x=pow(a,3)+pow(b,3)+pow(c,3);
+     if(x==n)
+     printf("Yes\n");
+     else
+     printf("No\n");
+    } 
+    return  0;
+}
