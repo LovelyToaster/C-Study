bc3dff8c9774c7b9fd2ea3d26174d45069bd79ce
LovelyToaster <92214385+LovelyToaster@users.noreply.github.com>
2021-10-25 21:16:04 +0800

    Case啥啥的

---
+++ C++-Homework-3-20211025.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int i,x;
+    i=1;
+    while(cin>>x&&x!=0)
+    {
+        printf("Case %d: %d\n",i,x);
+        ++i;
+    }
+    return 0;
+}
