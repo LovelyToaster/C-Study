b0f6149aafdb29f7d0ce50c34cf0c57a97d27bf1
LovelyToaster <92214385+LovelyToaster@users.noreply.github.com>
2021-10-29 11:27:49 +0800

    计算圆的面积和周长

---
+++ C++/C++-Homework-9-20211026.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define M_PI 3.14159265358979323846264338327950288
+int main()
+{
+    double r;
+    cin>>r;
+    printf("%.6lf %.6lf\n",M_PI*r*r,2*M_PI*r);
+    return 0;
+}
