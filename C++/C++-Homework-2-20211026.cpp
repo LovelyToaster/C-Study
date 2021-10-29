b32063ba577406a9c1204651049b4f4f4453d535
LovelyToaster <92214385+LovelyToaster@users.noreply.github.com>
2021-10-29 11:21:23 +0800

    子序列的和

--- C++/C++-Homework-2-20211026.cpp
+++ C++/C++-Homework-2-20211026.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 int main()
 {
-    int m,i;
-    double T;
-    T=1;
-    cin>>m;
-    for(i=2;i<=m;i++)
-    T=T-1.0/(i*i);
-    printf("%.6f",T);
+    int n,m,i;
+    double S;   
+    while (cin>>n>>m)
+    {
+        S=0;
+        for(i=n;i<=m;i++)
+        S=S+1.0/i/i;
+        printf("%.5lf\n",S);
+    }
     return 0;
 }
