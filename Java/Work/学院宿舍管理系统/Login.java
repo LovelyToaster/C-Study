d8fc609dce4d6e16a20172f84910a46735f55923
lovelytoaster94 <lovelytoaster94@outlook.com>
2023-06-12 00:00:01 +0800

    2023/06/12

--- Java/Work/学院宿舍管理系统/Login.java
+++ Java/Work/学院宿舍管理系统/Login.java
@@ -1,52 +1,62 @@
+package 学院宿舍管理系统;
+
+import java.io.*;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Scanner;
 import java.util.Set;
 
 public class Login {
     String name;
     String password;
-    HashMap<String,String> login_map=new HashMap<String,String>(); //保存用户账号密码
-    public void password_verify() { //账号密码验证
+    HashMap<String, String> login_map = new HashMap<>(); // 保存用户账号密码
+
+    public void password_verify() { // 账号密码验证
         int i = 0;
         login_map.put("zhu", "123456");
         Scanner sc = new Scanner(System.in);
         Set<String> user = login_map.keySet();
         System.out.print("输入用户名：");
         String name = sc.next();
-        String thisname=null;
-        while (i==0) {
-            Iterator<String> user_iterator = user.iterator();
-            while (user_iterator.hasNext()) {
-                thisname = user_iterator.next();
+        String thisname = null;
+        while (i == 0) {
+            for (String s : user) {
+                thisname = s;
                 if (name.equals(thisname)) {
-                    i=1;
+                    i = 1;
                     break;
                 }
             }
-            if(i==0) {
+            if (i == 0) {
                 System.out.println("这里返回账号错误，要求重新输入");
                 name = sc.next();
             }
         }
         System.out.print("输入密码：");
-        String password=sc.next();
-        String thispassword= login_map.get(thisname);
-        while(!password.equals(thispassword)) {
+        String password = sc.next();
+        String thispassword = login_map.get(thisname);
+        while (!password.equals(thispassword)) {
             System.out.println("这里返回密码错误，要求重新输入");
             password = sc.next();
         }
 
     }
-    public void user_register(){ //注册
-        Scanner sc=new Scanner(System.in);
+
+    public void user_register() { // 注册
+        Scanner sc = new Scanner(System.in);
         System.out.println("注册");
         login_map.put(sc.next(), sc.next());
     }
-    public void user_in(){ //用户密码写入文件
 
+    public void user_in() throws IOException { // 用户密码写入文件
+        FileOutputStream fos = new FileOutputStream("password");
+        ObjectOutputStream oos = new ObjectOutputStream(fos);
+        oos.writeObject(login_map);
     }
-    public void user_out(){ //用户密码写出文件
+
+    public void user_out() throws IOException, ClassNotFoundException { // 用户密码写出文件
+        FileInputStream fis = new FileInputStream("password");
+        ObjectInputStream ois = new ObjectInputStream(fis);
+        login_map = (HashMap<String, String>) ois.readObject();
 
     }
 }
--- Java/Work/学院宿舍管理系统/Main.java
+++ Java/Work/学院宿舍管理系统/Main.java
@@ -1,16 +1,21 @@
 package 学院宿舍管理系统;
 
-import java.util.ArrayList;
+import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         Login login = new Login();
-        if (true)
-            login.password_verify();
-        ArrayList<Student> student_manage = new ArrayList<>();
-        Student stu = new Student();
-        student_manage.add(stu.addstudent());
-        stu = student_manage.get(0);
-        stu.getstudent();
+        login.user_out();
+        // login.user_out();
+        // login.user_register();
+        login.password_verify();
+        /*
+         * ArrayList<Student> student_manage = new ArrayList<>();
+         * Student stu = new Student();
+         * student_manage.add(stu.addstudent());
+         * stu = student_manage.get(0);
+         * stu.getstudent();
+         */
+        login.user_in();
     }
 }
